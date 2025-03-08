5e5ce333a0429e0a8b1ae3598873f1c61334a5c2
ezepsosa <ezequielperez50@gmail.com>
2025-03-08 17:21:05 +0100

    Add User  and Product models

---
+++ marcus-bike-api/src/main/java/com/ezepsosa/models/Product.java
@@ -0,0 +1,38 @@
+package com.ezepsosa.models;
+
+public class Product {
+
+    private Long id;
+    private String productName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Product() {
+    }
+
+    public Product(Long id, String productName) {
+        this.id = id;
+        this.productName = productName;
+    }
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+
+    // Method for debugging and logs
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", productName=" + productName + "]";
+    }
+
+}
---
+++ marcus-bike-api/src/main/java/com/ezepsosa/models/User.java
@@ -0,0 +1,70 @@
+package com.ezepsosa.models;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String passwordHash;
+    private UserRol rol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public UserRol getRol() {
+        return rol;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public void setRol(UserRol rol) {
+        this.rol = rol;
+    }
+
+    public User() {
+    }
+
+    public User(String username, String email, String passwordHash, UserRol rol) {
+        this.username = username;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    public User(Long id, String username, String email, String passwordHash, UserRol rol) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.rol = rol;
+    }
+
+    // Method for debugging and logs
+    @Override
+    public String toString() {
+        return "AppUser [id=" + id + ", username=" + username + ", email=" + email + ", passwordHash=" + passwordHash
+                + ", rol=" + rol + "]";
+    }
+
+}
