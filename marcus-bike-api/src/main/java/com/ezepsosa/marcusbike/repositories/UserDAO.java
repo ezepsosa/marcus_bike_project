c89b0fc94bea4e149f83bcbcce1d4db0e665cf84
ezepsosa <ezequielperez50@gmail.com>
2025-03-09 20:02:37 +0100

    Add User DAO layer

---
+++ marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/repositories/UserDAO.java
@@ -0,0 +1,129 @@
+package com.ezepsosa.marcusbike.repositories;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ezepsosa.marcusbike.config.HikariDatabaseConfig;
+import com.ezepsosa.marcusbike.models.User;
+import com.ezepsosa.marcusbike.models.UserRole;
+
+public class UserDAO {
+
+    public User getById(Long id) {
+        String SQL_GET_ID_QUERY = "SELECT * FROM app_user WHERE id = (?)";
+
+        try (Connection connection = HikariDatabaseConfig.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(SQL_GET_ID_QUERY);
+
+            pst.setLong(1, id);
+            ResultSet rs = pst.executeQuery();
+
+            while (rs.next()) {
+                return new User(rs.getLong("id"), rs.getString("username"), rs.getString("email"),
+                        rs.getString("password_hash"), UserRole.valueOf(rs.getString("user_role").toUpperCase()),
+                        rs.getTimestamp("created_at").toLocalDateTime());
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public List<User> getAll() {
+        List<User> users = new ArrayList<User>();
+        String SQL_GET_ALL_QUERY = "SELECT * FROM app_user";
+
+        try (Connection connection = HikariDatabaseConfig.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(SQL_GET_ALL_QUERY);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                User userResult = new User(rs.getLong("id"), rs.getString("username"), rs.getString("email"),
+                        rs.getString("password_hash"), UserRole.valueOf(rs.getString("user_role").toUpperCase()),
+                        rs.getTimestamp("created_at").toLocalDateTime());
+                users.add(userResult);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public Long insert(User user) {
+
+        String SQL_INSERT_QUERY = "INSERT INTO app_user (username, email, password_hash, user_role) VALUES (?, ?, ?, ?::user_role) RETURNING id";
+
+        try (Connection connection = HikariDatabaseConfig.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(SQL_INSERT_QUERY,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+
+            pst.setString(1, user.getUsername());
+            pst.setString(2, user.getEmail());
+            pst.setString(3, user.getPasswordHash());
+            pst.setString(4, user.getRole().name().toLowerCase());
+
+            Integer affectedRows = pst.executeUpdate();
+
+            if (affectedRows > 0) {
+                try (ResultSet generatedValues = pst.getGeneratedKeys()) {
+                    if (generatedValues.next()) {
+                        return generatedValues.getLong(1);
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public Boolean update(User user) {
+        String SQL_UPDATE_QUERY = "UPDATE app_user SET username = ?, email = ?, password_hash = ?, user_role = ?::user_role WHERE id = ?";
+
+        try (Connection connection = HikariDatabaseConfig.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_QUERY);
+
+            pst.setString(1, user.getUsername());
+            pst.setString(2, user.getEmail());
+            pst.setString(3, user.getPasswordHash());
+            pst.setString(4, user.getRole().name().toLowerCase());
+            pst.setLong(5, user.getId());
+
+            Integer affectedRows = pst.executeUpdate();
+            return affectedRows > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+    public Boolean delete(Long id) {
+
+        String SQL_DELETE_QUERY = "DELETE FROM app_user WHERE id = (?)";
+
+        try (Connection connection = HikariDatabaseConfig.getConnection()) {
+            PreparedStatement pst = connection.prepareStatement(SQL_DELETE_QUERY,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+
+            pst.setLong(1, id);
+            Integer affectedRows = pst.executeUpdate();
+
+            return affectedRows > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+
+    }
+
+}
