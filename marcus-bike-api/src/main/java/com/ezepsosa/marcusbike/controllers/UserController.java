2f59623d6d82f0b6741c9436e8c0e36f0be1004c
ezepsosa <ezequielperez50@gmail.com>
2025-03-10 18:46:33 +0100

    Add get, getById, and insert methods in UserController

--- marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/controllers/UserController.java
+++ marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/controllers/UserController.java
@@ -2,10 +2,15 @@ package com.ezepsosa.marcusbike.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.ezepsosa.marcusbike.models.User;
 import com.ezepsosa.marcusbike.routes.RouteRegistrar;
 import com.ezepsosa.marcusbike.services.UserService;
 import com.ezepsosa.marcusbike.utils.JsonResponseUtil;
+import com.ezepsosa.marcusbike.utils.RequestUtils;
+import com.ezepsosa.marcusbike.utils.ValidatorUtils;
 
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.RoutingHandler;
@@ -14,19 +19,71 @@ import io.undertow.util.Methods;
 public class UserController implements RouteRegistrar {
 
     private UserService userService;
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    public void getAll(HttpServerExchange exchange) {
-        List<User> users = userService.getAll();
-        JsonResponseUtil.sendJsonResponse(exchange, users);
-    }
-
     @Override
     public void registerRoutes(RoutingHandler router) {
         router.add(Methods.GET, "/users", this::getAll);
+        router.add(Methods.GET, "/users/{id}", this::getById);
+        router.add(Methods.POST, "/users", this::insert);
+
+    }
+
+    public void getAll(HttpServerExchange exchange) {
+        logger.info("Received request: GET /users");
+        List<User> users = userService.getAll();
+        JsonResponseUtil.sendJsonResponse(exchange, users);
+        logger.info("Responded with {} users", users.size());
+
+    }
+
+    public void getById(HttpServerExchange exchange) {
+        logger.info("Received request: GET /users/{id}");
+
+        Long userId = RequestUtils.getRequestParam(exchange, "id");
+        if (userId == null) {
+            logger.warn("Invalid or missing user ID");
+            JsonResponseUtil.sendErrorResponse(exchange, 400, "Invalid or missing user ID");
+        }
+        logger.info("Fetching user with ID {}", userId);
+
+        User user = userService.getById(userId);
+        if (user == null) {
+            logger.warn("User with ID {} not found", userId);
+            JsonResponseUtil.sendErrorResponse(exchange, 404, "User not found");
+        }
+        logger.info("User with ID {} found", userId);
+        JsonResponseUtil.sendJsonResponse(exchange, user);
+    }
+
+    public void insert(HttpServerExchange exchange) {
+        exchange.getRequestReceiver().receiveFullBytes((ex, message) -> {
+            try {
+                User userToInsert = JsonResponseUtil.parseJson(message, User.class);
+
+                if (!ValidatorUtils.validateUser(userToInsert)) {
+                    logger.error("Error validating user");
+                    JsonResponseUtil.sendErrorResponse(exchange, 400, "Invalid user format or missing fields");
+                }
+                Long userId = userService.insert(userToInsert);
+                if (userId == null) {
+                    logger.error("Error inserting user");
+                    JsonResponseUtil.sendErrorResponse(exchange, 500, "Failed to insert user");
+                }
+                logger.info("User created with ID {}", userId);
+                userToInsert.setId(userId);
+                JsonResponseUtil.sendJsonResponse(exchange, userId);
+
+            } catch (Exception e) {
+                logger.error("Error processing request", e);
+                JsonResponseUtil.sendErrorResponse(exchange, 500, "Invalid request body");
+            }
+        });
+
     }
 
 }
