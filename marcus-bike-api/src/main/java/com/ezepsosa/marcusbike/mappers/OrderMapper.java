ab55c74fd6863bdb327af19c14e1b822314eaf2e
ezepsosa <ezequielperez50@gmail.com>
2025-03-11 21:02:59 +0100

    Add get by order method on controller. Add proper Mapping

--- marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/mappers/OrderMapper.java
+++ marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/mappers/OrderMapper.java
@@ -1,17 +1,17 @@
 package com.ezepsosa.marcusbike.mappers;
 
+import java.util.List;
 import java.util.stream.Collectors;
 
 import com.ezepsosa.marcusbike.dto.OrderDTO;
 import com.ezepsosa.marcusbike.models.Order;
+import com.ezepsosa.marcusbike.models.OrderLine;
 
 public class OrderMapper {
 
-    public static OrderDTO toDTO(Order order) {
+    public static OrderDTO toDTO(Order order, List<OrderLine> orderLine) {
         return new OrderDTO(order.getId(), order.getFinalPrice(), order.getCreatedAt(),
-                order.getOrderLines().stream().map(orderLine -> OrderLineMapper.toDTO(orderLine))
-                        .collect(Collectors.toList()));
+                orderLine.stream().map(OrderLineMapper::toDTO).collect(Collectors.toList()));
     }
 
 }
-//
