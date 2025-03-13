b84b3fbe4bbe597b4c0386908916850bace7fc39
ezepsosa <ezequielperez50@gmail.com>
2025-03-13 14:58:22 +0100

    Refactor on OrderLineProductPartDAO becasue quantity is no longer necessary

--- marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/repositories/OrderLineProductPartDAO.java
+++ marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/repositories/OrderLineProductPartDAO.java
@@ -22,8 +22,8 @@ public class OrderLineProductPartDAO {
 
     private final String SQL_GET_ALL_QUERY = "SELECT olpp.*, pp.id AS product_part_id, pp.part_option, pp.is_available, pp.base_price, pp.category, pp.created_at AS product_part_created_at, p.id AS product_id, p.product_name, p.created_at AS product_created_at FROM order_line_product_part olpp JOIN product_part pp ON olpp.product_part_id = pp.id JOIN product p ON pp.product_id = p.id";
     private final String SQL_GET_ID_QUERY = "SELECT olpp.*, pp.id AS product_part_id, pp.part_option, pp.is_available, pp.base_price, pp.category, pp.created_at AS product_part_created_at, p.id AS product_id, p.product_name, p.created_at AS product_created_at FROM order_line_product_part olpp JOIN product_part pp ON olpp.product_part_id = pp.id JOIN product p ON pp.product_id = p.id WHERE olpp.order_line_id = ? AND olpp.product_part_id = ?";
-    private final String SQL_INSERT_QUERY = "INSERT INTO order_line_product_part(order_line_id, product_part_id, quantity, final_price) VALUES (?, ?, ?, ?) RETURNING order_line_id AND";
-    private final String SQL_UPDATE_QUERY = "UPDATE order_line_product_part SET quantity = ?, final_price = ? WHERE order_line_id = ? AND product_part_id = ?";
+    private final String SQL_INSERT_QUERY = "INSERT INTO order_line_product_part(order_line_id, product_part_id, final_price) VALUES (?, ?, ?, ?) RETURNING order_line_id AND";
+    private final String SQL_UPDATE_QUERY = "UPDATE order_line_product_part final_price = ? WHERE order_line_id = ? AND product_part_id = ?";
     private final String SQL_DETELE_QUERY = "DELETE FROM order_line_product_part WHERE order_line_id = ? AND product_part_id = ?";
     private final String SQL_GET_ALL_BY_ORDERLINE_QUERY = "SELECT olpp.*, pp.id AS product_part_id, pp.part_option, pp.is_available, pp.base_price, pp.category, pp.created_at AS product_part_created_at, p.id AS product_id, p.product_name, p.created_at AS product_created_at FROM order_line_product_part olpp JOIN product_part pp ON olpp.product_part_id = pp.id JOIN product p ON pp.product_id = p.id where olpp.order_line_id = ?";
 
@@ -64,8 +64,7 @@ public class OrderLineProductPartDAO {
                 PreparedStatement pst = connection.prepareStatement(SQL_INSERT_QUERY)) {
             pst.setLong(1, orderLineProductPart.getOrderLine().getId());
             pst.setLong(2, orderLineProductPart.getProductPart().getId());
-            pst.setInt(3, orderLineProductPart.getQuantity());
-            pst.setDouble(4, orderLineProductPart.getFinalPrice());
+            pst.setDouble(3, orderLineProductPart.getFinalPrice());
 
             return pst.executeUpdate() > 0;
         } catch (SQLException e) {
@@ -78,10 +77,9 @@ public class OrderLineProductPartDAO {
     public Boolean update(OrderLineProductPart orderLineProductPart) {
         try (Connection connection = HikariDatabaseConfig.getConnection();
                 PreparedStatement pst = connection.prepareStatement(SQL_UPDATE_QUERY)) {
-            pst.setInt(1, orderLineProductPart.getQuantity());
-            pst.setDouble(2, orderLineProductPart.getFinalPrice());
-            pst.setLong(3, orderLineProductPart.getOrderLine().getId());
-            pst.setLong(4, orderLineProductPart.getProductPart().getId());
+            pst.setDouble(1, orderLineProductPart.getFinalPrice());
+            pst.setLong(2, orderLineProductPart.getOrderLine().getId());
+            pst.setLong(3, orderLineProductPart.getProductPart().getId());
 
             return pst.executeUpdate() > 0;
         } catch (SQLException e) {
@@ -140,7 +138,6 @@ public class OrderLineProductPartDAO {
         return new OrderLineProductPart(
                 null,
                 productPart,
-                rs.getInt("quantity"),
                 rs.getDouble("final_price"),
                 rs.getTimestamp("created_at").toLocalDateTime());
     }
