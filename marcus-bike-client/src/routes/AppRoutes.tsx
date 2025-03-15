037396a54fc03f1330cce5f81fe432d178240bb0
ezepsosa <ezequielperez50@gmail.com>
2025-03-15 11:27:40 +0100

    Add structure for layouts

--- marcus-bike-client/src/routes/AppRoutes.tsx
+++ marcus-bike-client/src/routes/AppRoutes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
+import { Layout } from "../components/Layout/Layout";
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<App />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
