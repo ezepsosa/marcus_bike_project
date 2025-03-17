13b02cfc884821467022b3fa865003da983ea707
ezepsosa <ezequielperez50@gmail.com>
2025-03-17 16:07:37 +0100

    Add BACKEND Cors methods

--- marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/routes/CorsHandler.java
+++ marcus-bike-api/src/main/java/com/ezepsosa/marcusbike/routes/CorsHandler.java
@@ -14,7 +14,15 @@ public class CorsHandler implements HttpHandler {
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
         exchange.getResponseHeaders().put(new HttpString("Access-Control-Allow-Origin"), "http://localhost:5173");
+        exchange.getResponseHeaders().put(new HttpString("Access-Control-Allow-Methods"), "GET, PUT, DELETE, OPTIONS");
+        exchange.getResponseHeaders().put(new HttpString("Access-Control-Allow-Headers"),
+                "Content-Type, Authorization");
 
+        if (exchange.getRequestMethod().equals(new HttpString("OPTIONS"))) {
+            exchange.setStatusCode(204);
+            exchange.endExchange();
+            return;
+        }
         next.handleRequest(exchange);
     }
 }
