6d568ec05cdec0b935f91c39f0e95ab5d57a5b6b
ezepsosa <ezequielperez50@gmail.com>
2025-04-03 16:26:29 +0200

    Refactor User controller to use proper DTO

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/controllers/UserController.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/controllers/UserController.java
@@ -3,7 +3,7 @@ package com.marcusbike.marcus_bike_api.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marcusbike.marcus_bike_api.dto.UserDTO;
+import com.marcusbike.marcus_bike_api.dto.response.UserResponseDTO;
 import com.marcusbike.marcus_bike_api.services.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserDTO> getMethodName() {
+    public List<UserResponseDTO> getMethodName() {
         return userService.findAll();
     }
 
