34a93adb6e9e6194ff90e9df808c5de0896e3250
ezepsosa <ezequielperez50@gmail.com>
2025-04-04 16:53:49 +0200

    Add flyway configuration

--- marcus-bike-api/src/main/resources/db/migration/V2__initial_data.sql
+++ marcus-bike-api/src/main/resources/db/migration/V2__initial_data.sql
@@ -1,3 +1,3 @@
 INSERT INTO app_user (username, email, password_hash, user_role) VALUES
-('marcus', 'marcus@admin.com', 'password_hash_1', 'admin'),
-('john', 'johndoe@example.com', 'password_hash_2', 'user');
+('marcus', 'marcus@admin.com', '$2a$10$RGL9y/.CmD5lvuFx4tA2YurkTAacEYsJWQD5o3cSWQ6bRdIx8AHB.', 'ADMIN'),
+('john', 'johndoe@example.com', '$2a$10$RGL9y/.CmD5lvuFx4tA2YurkTAacEYsJWQD5o3cSWQ6bRdIx8AHB.', 'USER');
