46ce5355c593362e3fb99052b221d1a116fdb4e6
ezepsosa <ezequielperez50@gmail.com>
2025-04-04 16:54:20 +0200

    Add enabled security method

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/config/SecurityConfig.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/config/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.marcusbike.marcus_bike_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
