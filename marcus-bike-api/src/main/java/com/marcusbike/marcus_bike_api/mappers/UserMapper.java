096e16cd2b3340f1b956ce8edfbbc20101173809
ezepsosa <ezequielperez50@gmail.com>
2025-04-11 13:05:57 +0200

    Refactor User mapper

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/mappers/UserMapper.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/mappers/UserMapper.java
@@ -5,10 +5,10 @@ import com.marcusbike.marcus_bike_api.models.User;
 
 public class UserMapper {
 
-    public static UserResponseDTO toDTO(User user) {
-        return UserResponseDTO.builder().id(user.getId()).email(user.getEmail()).username(user.getUsername()).password(user.getPassword())
-                .role(user.getRole().name()).build();
+	public static UserResponseDTO toDTO(User user) {
+		return UserResponseDTO.builder().id(user.getId()).email(user.getEmail()).username(user.getUsername())
+				.password(user.getPassword()).role(user.getRole().name()).build();
 
-    }
+	}
 
 }
