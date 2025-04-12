94598318fbc5240342a3771abcbb19fbca408d8b
ezepsosa <ezequielperez50@gmail.com>
2025-04-12 18:23:50 +0200

    Add missing brand property on product

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/models/Product.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/models/Product.java
@@ -29,11 +29,14 @@ public class Product {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(nullable = false)
-
 	private String productName;
+	
 	@Column(nullable = false)
-
+	private String brand;
+	
+	@Column(nullable = false)
 	private String category;
 
 	@Column(nullable = false)
