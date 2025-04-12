b794cad71cac643885bbc47974e2c1916f7e8cbd
ezepsosa <ezequielperez50@gmail.com>
2025-04-12 18:26:42 +0200

    Clean code

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/dto/response/ProductDetailResponseDTO.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/dto/response/ProductDetailResponseDTO.java
@@ -3,14 +3,6 @@ package com.marcusbike.marcus_bike_api.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.marcusbike.marcus_bike_api.models.ProductPartAssociation;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Builder;
 import lombok.Value;
 
