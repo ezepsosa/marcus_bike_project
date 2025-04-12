be61c7d4bb8d325bdaeed1566d7bada40615d49b
ezepsosa <ezequielperez50@gmail.com>
2025-04-12 18:24:38 +0200

    Add post product methods

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/controllers/ProductController.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/controllers/ProductController.java
@@ -1,18 +1,25 @@
 package com.marcusbike.marcus_bike_api.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcusbike.marcus_bike_api.dto.request.ProductRequestDTO;
 import com.marcusbike.marcus_bike_api.dto.response.ProductDetailResponseDTO;
 import com.marcusbike.marcus_bike_api.dto.response.ProductSummaryResponseDTO;
 import com.marcusbike.marcus_bike_api.services.ProductService;
+import com.marcusbike.marcus_bike_api.validations.ValidationSequence;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,19 +33,33 @@ public class ProductController {
 	@GetMapping
 	public ResponseEntity<List<ProductSummaryResponseDTO>> findAllSummary() {
 		List<ProductSummaryResponseDTO> response = this.productService.findAllSummary();
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping("/detailed")
 	public ResponseEntity<List<ProductDetailResponseDTO>> findAllDetail() {
 		List<ProductDetailResponseDTO> response = this.productService.findAllDetail();
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseEntity<ProductDetailResponseDTO> findById(@PathVariable Long id) {
 		ProductDetailResponseDTO response = this.productService.findById(id);
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+		return ResponseEntity.status(HttpStatus.OK).body(response);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+		this.productService.deleteById(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+
+	@PostMapping
+	public ResponseEntity<ProductSummaryResponseDTO> create(@RequestBody @Validated(ValidationSequence.class) ProductRequestDTO productRequest) {
+		ProductSummaryResponseDTO response = this.productService.create(productRequest);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(response.getId())
+				.toUri();
+		return ResponseEntity.created(location).body(response);
 	}
 
 }
--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/services/ProductService.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/services/ProductService.java
@@ -3,8 +3,10 @@ package com.marcusbike.marcus_bike_api.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
-
+import com.marcusbike.marcus_bike_api.dto.request.ProductRequestDTO;
 import com.marcusbike.marcus_bike_api.dto.response.ProductDetailResponseDTO;
 import com.marcusbike.marcus_bike_api.dto.response.ProductSummaryResponseDTO;
 import com.marcusbike.marcus_bike_api.mappers.ProductMapper;
@@ -12,6 +14,7 @@ import com.marcusbike.marcus_bike_api.models.Product;
 import com.marcusbike.marcus_bike_api.repositories.ProductRepository;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,8 +35,28 @@ public class ProductService {
 
 	public ProductDetailResponseDTO findById(Long id) {
 		Product product = this.productRepository.findById(id)
-				.orElseThrow(() -> new EntityNotFoundException("\"No product found with id: {}\", id"));
+				.orElseThrow(() -> new EntityNotFoundException(String.format("No product found with id: %d", id)));
 		return ProductMapper.toDetailDTO(product);
+	}
+
+	@Transactional
+	public ProductSummaryResponseDTO create(ProductRequestDTO productDTO) {
+		Product product = ProductMapper.toEntity(productDTO);
+		Product res = this.productRepository.save(product);
+		return ProductMapper.toSummaryDTO(res);
+
+	}
+
+	@Transactional
+	public void deleteById(Long id) {
+		try {
+			this.productRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntityNotFoundException("No product found with ID " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException(
+					"Product cannot be deleted because it is being used by another entity");
+		}
 	}
 
 }
