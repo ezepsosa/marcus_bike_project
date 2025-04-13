bce6508358790b090a00b068c89bf328c175f570
ezepsosa <ezequielperez50@gmail.com>
2025-04-13 12:41:31 +0200

    Fix security filter

--- marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/config/SecurityConfig.java
+++ marcus-bike-api/src/main/java/com/marcusbike/marcus_bike_api/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.marcusbike.marcus_bike_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
